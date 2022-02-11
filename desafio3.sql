19562268a4244158d9b67ce29e9d70d75c9542f1
raffrasson <raf.frasson@gmail.com>
2022-02-10 22:08:18 -0300

    requisito 4

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,7 @@
-SELECT user_name AS 'usuario',
-COUNT(history.user_id) AS 'qtde_musicas_ouvidas',
-ROUND(SUM(songs.song_length) / 60,2) AS 'total_minutos' 
+SELECT 
+  user_name AS 'usuario',
+  COUNT(history.user_id) AS 'qtde_musicas_ouvidas',
+  ROUND(SUM(songs.song_length) / 60,2) AS 'total_minutos' 
 FROM SpotifyClone.users AS users
 
 INNER JOIN SpotifyClone.user_history AS history
---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+SELECT 
+  user.user_name AS 'usuario',
+  IF (YEAR(MAX(history.played_on)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS 'condicao_usuario'
+FROM SpotifyClone.user_history AS history
+
+INNER JOIN SpotifyClone.users AS user 
+ON history.user_id = user.user_id
+
+GROUP BY history.user_id
+ORDER BY user.user_name ASC;
